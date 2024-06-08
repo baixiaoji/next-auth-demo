cb7ef443f6fad983d2442615d5360b397d9a4082
baixiaoji <916019725@qq.com>
2024-06-08 19:48:01 +0800

    feat: 更新 nextauth 登录

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,25 @@
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+
+const authHandlers = NextAuth({
+    providers: [
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                email: { label: "Email", type: "text"},
+                password: { label: "Password", type: "password"}
+            },
+            async authorize(credentials) {
+                try {
+                    console.log(credentials)
+                    return null
+                } catch (e) {
+                    console.log(e);
+                }
+            }
+        })
+    ],
+})
+
+export { authHandlers as GET, authHandlers as POST}
